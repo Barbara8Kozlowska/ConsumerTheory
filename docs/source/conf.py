ab7b13387a3d209534d6d5b183331e08671a32ed
Barbara8Kozlowska <barbara.kozlowska@poczta.fm>
2026-06-10 19:04:28 +0200

    Create conf.py

---
+++ docs/source/conf.py
@@ -0,0 +1,17 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../.."))
+
+project = "ConsumerTheory"
+author = "Your Name"
+
+extensions = [
+    "sphinx.ext.mathjax",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon"
+]
+
+html_theme = "sphinx_rtd_theme"
+
+html_static_path = []
